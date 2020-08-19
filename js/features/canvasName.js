a85cdfcbdbc38e7d40480f44f3c915069ea0f712
JFL110 <typus24@hotmail.com>
2020-08-19 15:28:51 +0100

    Add mobile friendly url modal

--- js/features/canvasName.js
+++ js/features/canvasName.js
@@ -1,13 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Frame from './frame';
 import canvasSlice from './canvasSlice';
 import config from './config'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons/faArrowRight'
+import useOnClickOutside from './useOnClickOutside'
 
 export default Frame.connectWithSlice(canvasSlice,
     ({ canvasId }) => {
 
+
+        const [modalOpen, setModalOpen] = useState(false);
         const [text, setText] = useState(canvasId);
         const stripSlashAndSet = v => {
             setText(v.replace(/^\/+/, ''));
@@ -22,14 +25,40 @@ export default Frame.connectWithSlice(canvasSlice,
             window.location.href = config.frontUrl + "/" + (text ?? "");
         }
 
-        return <div className="canvas-name">
-            <form onSubmit={onSubmit}>
-                <input
-                    type="text"
-                    value={"/" + (text ?? "")}
-                    onChange={e => stripSlashAndSet(e.target.value)}
-                />
-                <FontAwesomeIcon icon={faArrowRight} className="go-button" onClick={go} />
-            </form>
-        </div>
+        const modalBoxRef = useRef();
+        useOnClickOutside(modalBoxRef, () => {
+            setModalOpen(false);
+        });
+
+        return <React.Fragment>
+            {/* Mobile */}
+            <div className="square-button canvas-name-mobile-button" onClick={() => setModalOpen(true)}>
+                <FontAwesomeIcon icon={faArrowRight} className="go-button" />
+            </div>
+            {modalOpen && <div className="mobile-canvas-name-modal" >
+                <div>
+                    <div id="modal-box" ref={modalBoxRef}>
+                        <form onSubmit={onSubmit}>
+                            <input
+                                type="text"
+                                value={"/" + (text ?? "")}
+                                onChange={e => stripSlashAndSet(e.target.value)}
+                            />
+                            <FontAwesomeIcon icon={faArrowRight} className="go-button" onClick={go} />
+                        </form>
+                    </div>
+                </div>
+            </div>}
+            {/* Desktop */}
+            <div className="canvas-name">
+                <form onSubmit={onSubmit}>
+                    <input
+                        type="text"
+                        value={"/" + (text ?? "")}
+                        onChange={e => stripSlashAndSet(e.target.value)}
+                    />
+                    <FontAwesomeIcon icon={faArrowRight} className="go-button" onClick={go} />
+                </form>
+            </div>
+        </React.Fragment>
     });
