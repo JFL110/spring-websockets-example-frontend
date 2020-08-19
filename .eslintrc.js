f8b16e2a67b48b9b2684fba34a4e9a045f401164
JFL110 <typus24@hotmail.com>
2020-08-19 08:50:08 +0100

    Add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,1 @@
+module.exports = { "env": { "browser": true, "es2020": true }, "extends": [ "eslint:recommended", "plugin:react/recommended" ], "parserOptions": { "ecmaFeatures": { "jsx": true }, "ecmaVersion": 11, "sourceType": "module" }, "plugins": [ "react" ], "rules": { "react/prop-types": 0, "react/display-name": 0 }, "settings": { "react": { "version": "detect" } } };
