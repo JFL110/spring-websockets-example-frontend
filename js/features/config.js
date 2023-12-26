b8367dbf1d44530ceac31cd31d3ad457cce97cc4
JFL110 <typus24@hotmail.com>
2023-12-26 19:36:40 +0000

    New BE URL

--- js/features/config.js
+++ js/features/config.js
@@ -1,5 +1,5 @@
 export default ({
     frontUrl: "http://d1kzdlgex69htr.cloudfront.net",
-    backUrl: " http://jamesleach-dev-alb-991674691.eu-west-2.elb.amazonaws.com:8093",
+    backUrl: "http://jamesleach-dev-alb-995225578.eu-west-2.elb.amazonaws.com:8093",
     // backUrl : "http://localhost:8083"
 })
