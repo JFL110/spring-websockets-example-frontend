64c671ac973cc2c3cfa6040c656b4441829ae6b6
JamesL <typus24@hotmail.com>
2025-02-02 20:02:39 +0000

    New BE URL

--- js/features/config.js
+++ js/features/config.js
@@ -1,5 +1,5 @@
 export default ({
     frontUrl: "http://d1kzdlgex69htr.cloudfront.net",
-    backUrl: "http://jamesleach-dev-alb-995225578.eu-west-2.elb.amazonaws.com:8083",
+    backUrl: "http://jamesleach-dev-alb-1652077306.eu-west-2.elb.amazonaws.com:8083",
     // backUrl : "http://localhost:8083"
 })
